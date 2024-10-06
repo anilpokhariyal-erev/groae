9755cc32f653924e3c973a5bd8836694158a8232
Anil Pokhariyal <anil.pokhariyal1@gmail.com>
2024-10-06 23:22:50 +0530

    txt updaed in package view

--- resources/views/frontend/packagedetail.blade.php
+++ resources/views/frontend/packagedetail.blade.php
@@ -5,8 +5,9 @@
                 <div class="backBtn ">
                     <a class="backAnchor" href="{{ url()->previous() }}">
                         <img src="{{ secure_asset('images/cheveron-right.png') }}" alt="">
+                        <h2 class="backTxt">Back</h2>
                     </a>
-                    <h2 class="backTxt">Back</h2>
+                    
                 </div>
                 <div class="topHeading">
                     <h2 class="trendTxt">Summary</h2>
