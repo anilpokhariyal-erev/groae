335ee85df90cc265ae61a63f6a7cc59f4601e949
Anil Pokhariyal <anil.pokhariyal1@gmail.com>
2024-11-02 18:53:32 +0530

    label validation fixed

--- resources/views/admin/attributes/edit.blade.php
+++ resources/views/admin/attributes/edit.blade.php
@@ -25,7 +25,7 @@
 
                         <div class="position-relative form-group">
                             <label for="label">Label <span class="text-danger">*</span></label>
-                            <input name="label" id="label" type="text" value="{{ old('label', $attribute->label) }}" class="form-control" onkeyup="checkInput(this, 'label')">
+                            <input name="label" id="label" type="text" value="{{ old('label', $attribute->label) }}" class="form-control">
                             <x-input-error class="mt-2 text-red" :messages="$errors->get('label')" />
                             <div id="label-suggestions" class="suggestions-box"></div>
                         </div>
