1f287b2fdfb3f1e10ba23e3c5ad51a0de651a8ff
Saurav Sharma <saurav.sharma@bjshomedelivery.com>
2024-11-17 15:15:35 +0530

    Update calculate_licensecost.blade.php

--- resources/views/frontend/cost_calculator/calculate_licensecost.blade.php
+++ resources/views/frontend/cost_calculator/calculate_licensecost.blade.php
@@ -105,8 +105,7 @@
                                                     return $line->attribute_id == $attribute->id && $line->attribute_option_id == $option->attributeOption->id;
                                                 });
                                             @endphp
-                                            <option data-val="{{ $option->attributeOption->value }}" value="{{ $option->attributeOption->id }}"
-                                                    {{ $selectedOption}}>
+                                            <option data-val="{{ $option->attributeOption->value }}" value="{{ $option->attributeOption->id }}">
                                                 {{ $option->attributeOption->value }}
                                             </option>
                                         @endforeach
