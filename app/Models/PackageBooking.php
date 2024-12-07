299d441923834bf62191ba92607a31d728b919a5
Anil Pokhariyal <anil.pokhariyal1@gmail.com>
2024-12-07 11:09:45 +0530

    payment remarks added

--- app/Models/PackageBooking.php
+++ app/Models/PackageBooking.php
@@ -20,7 +20,7 @@ class PackageBooking extends Model
         'status',
         'payment_method',
         'transaction_id',
-        'cancel_reason'
+        'remarks'
     ];
 
     // Relationships
