52cb98ed71ff6d0ae1167ffeaf392acb50279fdf
Anil Pokhariyal <anil.pokhariyal1@gmail.com>
2025-01-10 12:05:24 +0530

    email verification template updated

--- app/Mail/VerificationCodeMail.php
+++ app/Mail/VerificationCodeMail.php
@@ -40,28 +40,16 @@ class VerificationCodeMail extends Mailable
      */
     public function content(): Content
     {
-        // Set the header and body content for the template
-        $headerText = "Welcome To GroAE!";
-        $bodyText = "Hi {$this->name},<br><br>";
-        $bodyText .= "Thank you for joining Groae! We are thrilled to<br>";
-        $bodyText .= "have you with us. To Complete your registration,<br>";
-        $bodyText .= "please verify your email address by clicking the <br>";
-        $bodyText .= "button below:<br><br>";
-        $bodyText .= "<a href='{$this->verificationCode}'><button style=\"background-color: #304a6f; color: white; border: none; padding: 10px 20px; cursor: pointer; border-radius: 5px;\">Verify My Email</button></a><br>";
-        $bodyText .= "If you did not create an account, no action is<br>";
-        $bodyText .= "required, and you can safely ignore this email<br>";
-        $bodyText .= "Need help ? Feel free to contact us anytime.<br>";
-        $bodyText .= "<b>Regards</b>,<br>";
-        $bodyText .= "<b>Team GroAE</b>";
-
         return new Content(
-            view: 'frontend.email.email_template', // Ensure this points to your template
+            view: 'frontend.email.email_verification',
             with: [
-                'headerText' => $headerText,
-                'bodyText' => $bodyText,
+                'app_url' => config('app.url'),
+                'name' => $this->name,
+                'verificationCode' => $this->verificationCode,
             ],
         );
     }
+
 
     /**
      * Get the attachments for the message.
